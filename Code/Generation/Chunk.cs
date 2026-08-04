2b7e5f8c3bb8c78df04f6edb1513871417d9ca20
Jacob Isailofski <jacob.isailofski@gmail.com>
2026-08-04 18:08:28 +1000

    Moved some chunk/tile code

---
+++ Code/Generation/Chunk.cs
@@ -0,0 +1,36 @@
+﻿namespace Sandbox.Generation;
+
+public class Chunk
+{
+	public int ChunkX;
+	public int ChunkY;
+
+	public int WorldWidth;
+	public int WorldHeight;
+	
+	public int TileCount => _size * _size;
+	
+	public double[] Elevations;
+	public int[] RegionIds;
+	public double[] Humidities;
+	public double[] Temperatures;
+	public int[] MaterialIds;
+	
+	private int _size;
+
+	public Chunk(int chunkX, int chunkY, GenerationSettings settings)
+	{
+		ChunkX = chunkX;
+		ChunkY = chunkY;
+		WorldWidth = settings.WorldWidth;
+		WorldHeight = settings.WorldHeight;
+		
+		_size = settings.ChunkGridSize;
+
+		Elevations = new double[_size];
+		RegionIds = new int[_size];
+		Humidities = new double[_size];
+		Temperatures = new double[_size];
+		MaterialIds = new int[_size];
+	}
+}
---
+++ Code/Generation/Tile.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Sandbox.Generation;
+
+public class Tile
+{
+	public GenerationSettings Settings;
+	
+	public (int x, int y) Position;
+	
+	// speedy immediate-y chunk data
+	public double Elevation;
+	public int RegionId;
+	public double Humidity;
+	public double Temperature;
+	public int MaterialId;
+
+	// tile data for later generation passes
+	public SubterraneanLayer Geology;
+	
+	public Tile( int globalX, int globalY, GenerationSettings settings )
+	{
+		Settings = settings;
+		Position = (globalX, globalY);
+		Elevation = GetElevation(globalX, globalY);
+		Geology = BuildGeology(globalX, globalY, Elevation);
+	}
+
+	/**
+     * PASS 1: ELEVATION EVALUATION
+     * Determine the seeded elevation value for the tile using the provided
+	 * Voronoi field structure along with given OpenSimplexNoise
+    */
+	private double GetElevation( int x, int y )
+	{
+		// clamp inside world
+		int globalX = Math.Max( -Settings.HalfWidth, Math.Min(Settings.HalfWidth, x) );
+		int globalY = Math.Max( -Settings.HalfHeight, Math.Min(Settings.HalfHeight, y) );
+
+		double elevation = BuildTectonicSuperstructure(globalX, globalY);
+		elevation = BuildNoisyGeologicalDetail(elevation);
+		
+		// return clamped elevation
+		return Math.Max( -1.0d, Math.Min( 1.0d, elevation ) );
+	}
+
+	private double BuildTectonicSuperstructure( int x, int y )
+	{
+		return 0.0d; // @TODO: Does nothing for now.
+	}
+
+	private double BuildNoisyGeologicalDetail( double elevation )
+	{
+		return elevation; // @TODO: Does nothing for now.
+	}
+
+	/**
+     * PASS 2: GEOLOGICAL EVALUATION
+     * Find the geological structure of the space underneath the tile, so we know
+	 * how to later fill it with biome data.
+    */
+	private SubterraneanLayer BuildGeology( int x, int y, double elevation )
+	{
+		return new SubterraneanLayer( 0, 0, BasementRockType.Basalt ); // @TODO: Does nothing for now
+	}
+
+	/**
+     * PASS X: DETERMINE TILE REGION
+     * Use the determinate data of a tile to find what region it belongs to.
+	 * @NOTE: currently only accepts elevation data.
+    */
+	private RegionId GetRegion( int elevation )
+	{
+		return Generation.RegionId.Unassigned; // @TODO: Does nothing for now.
+	}
+}
